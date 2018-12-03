81c36e57a2768fd7c0d0f3abcc15ab8e7fb32a28
Rodrigo de Andrade Lourenço <ctba_rodrigo@hotmail.com>
2018-12-03 18:10:32 -0200

    first commit

---
+++ udemy/angular/ES6/arrow_function.js
@@ -0,0 +1,19 @@
+var dobroDoValor = numero => {
+    return numero * 2;
+}
+
+var dobroDoValor = () => {
+    return 2 * 2;
+}
+
+var dobroDoValor = (num, num2) => {
+    return num * num2;
+}
+
+var dobroDoValor = num => num * 2;
+
+var dobroDoValor = num => {
+    let result = num * 2;
+    return reslt;
+}
+console.log(dobroDoValor(7));
